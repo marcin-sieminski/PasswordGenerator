4de0bf60f87419e32d667fd64839a155234c9ceb
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-04-10 12:24:27 +0200

    Add project files.

---
+++ PasswordGenerator.ViewModel/PasswordViewModel.cs
@@ -0,0 +1,36 @@
+﻿using PasswordGenerator.Common;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PasswordGenerator.ViewModel
+{
+    public class PasswordViewModel : INotifyPropertyChanged
+    {
+        private readonly PasswordModel _password;
+
+        public PasswordViewModel(PasswordModel password)
+        {
+            _password = password;
+        }
+
+        public string Password
+        {
+            get => _password.Password;
+            set
+            {
+                if (_password.Password != value)
+                {
+                    _password.Password = value;
+                    RaisePropertyChanged(nameof(Password));
+                }
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ PasswordGeneratorWinUiDesktop/PasswordGeneratorWinUiDesktop/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml;
+
+
+namespace PasswordGeneratorWinUiDesktop
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
